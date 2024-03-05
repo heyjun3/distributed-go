f0f92599429731e71f4254d1593f904898c2d166
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-03-05 22:01:41 +0900

    add tests

--- internal/loadbalance/resolver.go
+++ internal/loadbalance/resolver.go
@@ -39,7 +39,7 @@ func (r *Resolver) Build(
 		fmt.Sprintf(`{"loadBalancingConfig": [{"%s": {}}]}`, Name),
 	)
 	var err error
-	r.resolverConn, err = grpc.Dial(target.Endpoint(), dialOpts...)
+	r.resolverConn, err = grpc.Dial(target.URL.Host, dialOpts...)
 	if err != nil {
 		return nil, err
 	}
