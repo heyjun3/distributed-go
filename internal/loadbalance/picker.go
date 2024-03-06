8d0284390e9b12cd9a1329aa93f5e1cdd2261078
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-03-06 21:36:28 +0900

    add picker

---
+++ internal/loadbalance/picker.go
@@ -0,0 +1,38 @@
+package loadbalance
+
+import (
+	"strings"
+	"sync"
+	"sync/atomic"
+
+	"google.golang.org/grpc/balancer"
+	"google.golang.org/grpc/balancer/base"
+)
+
+var _ base.PickerBuilder = (*Picker)(nil)
+
+type Picker struct {
+	mu        sync.RWMutex
+	leader    balancer.SubConn
+	followers []balancer.SubConn
+	current   uint64
+}
+
+func (p *Picker) Build(buildInfo base.PickerBuildInfo) balancer.Picker {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	var followers []balancer.SubConn
+	for sc, scInfo := range buildInfo.ReadySCs {
+		isLeader := scInfo.
+			Address.
+			Attributes.
+			Value("is_leader").(bool)
+		if isLeader {
+			p.leader = sc
+			continue
+		}
+		followers = append(followers, sc)
+	}
+	p.followers = followers
+	return p
+}
