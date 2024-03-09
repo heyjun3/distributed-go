cd756bf2d155ad1e3588a9942e76acee955390c8
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-03-09 10:00:27 +0900

    add tests

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -11,6 +11,7 @@ import (
 	api "github.com/heyjun3/proglog/api/v1"
 	"github.com/heyjun3/proglog/internal/agent"
 	"github.com/heyjun3/proglog/internal/config"
+	"github.com/heyjun3/proglog/internal/loadbalance"
 	"github.com/stretchr/testify/require"
 	"github.com/travisjeffery/go-dynaport"
 	"google.golang.org/grpc"
@@ -88,6 +89,9 @@ func TestAgent(t *testing.T) {
 		},
 	)
 	require.NoError(t, err)
+
+	time.Sleep(3 * time.Second)
+
 	consumeResponse, err := leaderClient.Consume(
 		context.Background(),
 		&api.ConsumeRequest{
@@ -96,8 +100,6 @@ func TestAgent(t *testing.T) {
 	)
 	require.NoError(t, err)
 	require.Equal(t, consumeResponse.Record.Value, []byte("foo"))
-
-	time.Sleep(3 * time.Second)
 
 	followerClient := client(t, agents[1], peerTLSConfig)
 	consumeResponse, err = followerClient.Consume(
@@ -128,7 +130,11 @@ func client(t *testing.T, agent *agent.Agent, tlsConfig *tls.Config) api.LogClie
 	rpcAddr, err := agent.Config.RPCAddr()
 	require.NoError(t, err)
 
-	conn, err := grpc.Dial(rpcAddr, opts...)
+	conn, err := grpc.Dial(fmt.Sprintf(
+		"%s://%s",
+		loadbalance.Name,
+		rpcAddr,
+	), opts...)
 	require.NoError(t, err)
 
 	client := api.NewLogClient(conn)
--- internal/loadbalance/picker_test.go
+++ internal/loadbalance/picker_test.go
@@ -27,3 +27,61 @@ func TestPickerNoSubConnAvailable(t *testing.T) {
 		require.Nil(t, result.SubConn)
 	}
 }
+
+func TestPickerProducesToLeader(t *testing.T) {
+	picker, subConns := setupTest()
+	info := balancer.PickInfo{
+		FullMethodName: "/log.vX.Log/Produce",
+	}
+	for i := 0; i < 5; i++ {
+		gotPick, err := picker.Pick(info)
+		require.NoError(t, err)
+		require.Equal(t, subConns[0], gotPick.SubConn)
+	}
+}
+
+func TestPickerConsumesFromFollowers(t *testing.T) {
+	picker, subConns := setupTest()
+	info := balancer.PickInfo{
+		FullMethodName: "/log.vX.Log/Consume",
+	}
+	for i := 0; i < 5; i++ {
+		pick, err := picker.Pick(info)
+		require.NoError(t, err)
+		require.Equal(t, subConns[i%2+1], pick.SubConn)
+	}
+}
+
+func setupTest() (*loadbalance.Picker, []*subConn) {
+	var subConns []*subConn
+	buildInfo := base.PickerBuildInfo{
+		ReadySCs: make(map[balancer.SubConn]base.SubConnInfo),
+	}
+	for i := 0; i < 3; i++ {
+		sc := &subConn{}
+		addr := resolver.Address{
+			Attributes: attributes.New("is_leader", i == 0),
+		}
+		// 0番目のサブコネクションはリーダーです
+		sc.UpdateAddresses([]resolver.Address{addr})
+		buildInfo.ReadySCs[sc] = base.SubConnInfo{Address: addr}
+		subConns = append(subConns, sc)
+	}
+	picker := &loadbalance.Picker{}
+	picker.Build(buildInfo)
+	return picker, subConns
+}
+
+// subConnは、balancer.SubConnを実装している
+type subConn struct {
+	addrs []resolver.Address
+}
+
+func (s *subConn) UpdateAddresses(addrs []resolver.Address) {
+	s.addrs = addrs
+}
+func (s *subConn) GetOrBuildProducer(p balancer.ProducerBuilder) (balancer.Producer, func()) {
+	return nil, func() {}
+}
+func (s *subConn) Connect()  {}
+func (s *subConn) Shutdown() {}
